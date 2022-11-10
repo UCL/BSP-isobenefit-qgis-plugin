2d3a985a8d5209fa643ce003706081cd5ec1f901
Gareth Simons <garethsimons@me.com>
2022-11-10 21:00:18 +0000

    ~

--- futurb/futurb.py
+++ futurb/futurb.py
@@ -17,11 +17,30 @@
  *                                                                         *
  ***************************************************************************/
 """
-import os.path
+from __future__ import annotations
 
+import os.path
+from typing import Any, Callable
+
+from qgis.core import (
+    Qgis,
+    QgsCoordinateReferenceSystem,
+    QgsFeature,
+    QgsGeometry,
+    QgsLayerTreeGroup,
+    QgsMessageLog,
+    QgsProject,
+    QgsRasterBlock,
+    QgsRasterLayer,
+    QgsVectorLayer,
+    QgsWkbTypes,
+)
+from qgis.gui import QgisInterface, QgsFileWidget
 from qgis.PyQt.QtCore import QCoreApplication, QSettings, QTranslator, qVersion
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtWidgets import QAction, QDialog, QListWidgetItem, QToolBar, QWidget
+from shapely import geometry, wkt
+from shapely.geometry.polygon import orient
 
 # Import the code for the dialog
 from .futurb_dialog import FuturbDialog
@@ -33,7 +52,14 @@ from .resources import *
 class Futurb:
     """QGIS Plugin Implementation."""
 
-    def __init__(self, iface):
+    iface: QgisInterface
+    plugin_dir: str
+    dlg: FuturbDialog
+    actions: list[QAction]
+    menu: str
+    toolbar: QToolBar
+
+    def __init__(self, iface: QgisInterface):
         """Constructor.
 
         :param iface: An interface instance that will be passed to this class
@@ -48,17 +74,13 @@ class Futurb:
         # initialize locale
         locale = QSettings().value("locale/userLocale")[0:2]
         locale_path = os.path.join(self.plugin_dir, "i18n", "Futurb_{}.qm".format(locale))
-
         if os.path.exists(locale_path):
             self.translator = QTranslator()
             self.translator.load(locale_path)
-
             if qVersion() > "4.3.3":
                 QCoreApplication.installTranslator(self.translator)
-
         # Create the dialog (after translation) and keep reference
         self.dlg = FuturbDialog()
-
         # Declare instance attributes
         self.actions = []
         self.menu = self.tr("&Future Urban Growth test.")
@@ -66,8 +88,7 @@ class Futurb:
         self.toolbar = self.iface.addToolBar("Futurb")
         self.toolbar.setObjectName("Futurb")
 
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
+    def tr(self, message: str):
         """Get the translation for a string using Qt translation API.
 
         We implement this ourselves since we do not inherit QObject.
@@ -78,20 +99,19 @@ class Futurb:
         :returns: Translated version of message.
         :rtype: QString
         """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate("Futurb", message)
 
     def add_action(
         self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None,
+        icon_path: str,
+        text: str,
+        callback: Callable[[Any], Any],
+        enabled_flag: bool = True,
+        add_to_menu: bool = True,
+        add_to_toolbar: bool = True,
+        status_tip: str | None = None,
+        whats_this: str | None = None,
+        parent: QWidget | None = None,
     ):
         """Add a toolbar icon to the toolbar.
 
@@ -171,14 +191,110 @@ class Futurb:
         # remove the toolbar
         del self.toolbar
 
+    def extract_input_feature(self, selected_layer: QgsVectorLayer) -> QgsFeature | None:
+        """ """
+        # unpack the layer's features
+        layer_features: list[QgsFeature] = [sl for sl in selected_layer.getFeatures()]
+        # bail if no features
+        if not layer_features:
+            self.iface.messageBar().pushMessage(
+                "Error", "No features available on the provided layer.", level=Qgis.Critical
+            )
+            return None
+        # check for selected features
+        selected_feature: QgsFeature | None = None
+        selected_features: list[QgsFeature] = selected_layer.selectedFeatures()
+        if selected_features:
+            # bail if more than one selected
+            if len(selected_features) > 1:
+                self.iface.messageBar().pushMessage(
+                    "Error", "Please select only a single feature from the provided layer.", level=Qgis.Critical
+                )
+                return None
+            # otherwise selecte the single feature
+            selected_feature = selected_features[0]
+        # otherwise, if nothing has been selected, take a look at the layers features
+        else:
+            # bail if more than one feature
+            if len(layer_features) > 1:
+                self.iface.messageBar().pushMessage(
+                    "Error",
+                    "Multiple features on the provided layer. Please select a single feature.",
+                    level=Qgis.Critical,
+                )
+                return None
+            # otherwise, select the single feature
+            selected_feature = layer_features[0]
+        return selected_feature
+
     def run(self):
         """Run method that performs all the real work"""
+        layer_map: dict[str, dict[str, QgsVectorLayer]] = {}
+        loaded_layers = QgsProject.instance().mapLayers()
+        # clear layers from list
+        self.dlg.layers_list.clear()
+        # look through open layers and add
+        layer_key: str
+        qgis_layer: QgsVectorLayer
+        for layer_key, qgis_layer in loaded_layers.items():
+            # filter out vector layers of Polygon types
+            if isinstance(qgis_layer, QgsVectorLayer) and qgis_layer.geometryType() == QgsWkbTypes.PolygonGeometry:
+                layer_name = qgis_layer.name()
+                layer_map[layer_name] = {"layer_key": layer_key, "qgis_layer": qgis_layer}
+                QListWidgetItem(layer_name, parent=self.dlg.layers_list)
         # show the dialog
         self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
+        result: int = self.dlg.exec_()  # returns 1 if pressed
         if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+            print(self.baa)
+            file_path_dlg = QDialog()
+            file_path_dlg.resize(300, 50)
+            file_path_widget = QgsFileWidget(file_path_dlg)
+            file_path_widget.setStorageMode(QgsFileWidget.SaveFile)
+            file_path_dlg.show()
+            fp_result: int = file_path_dlg.exec_()
+            print(fp_result)
+
+            # expects a single selected item from the list of layers
+            selected: list[QListWidgetItem] = self.dlg.layers_list.selectedItems()
+            # bail if nothing selected
+            if not selected:
+                self.iface.messageBar().pushMessage(
+                    "Error", "Please select a layer from which to fetch the extents.", level=Qgis.Critical
+                )
+                return
+            # bail if more than one selected
+            if len(selected) > 1:
+                self.iface.messageBar().pushMessage(
+                    "Error", "Please select a single layer from which to fetch the extents.", level=Qgis.Critical
+                )
+                return
+            # get the selected layer
+            layer_name = selected[0].text()
+            canvas_crs: QgsCoordinateReferenceSystem = self.iface.mapCanvas().mapSettings().destinationCrs()
+            if canvas_crs.isGeographic():
+                self.iface.messageBar().pushMessage(
+                    "Error",
+                    "Please use a projected Coordinate Reference System, e.g. EPSG 27700 for BNG.",
+                    level=Qgis.Critical,
+                )
+                return
+            selected_layer = layer_map[layer_name]["qgis_layer"]
+            selected_layer.setCrs(canvas_crs)
+            # get the feature
+            selected_feature: QgsFeature = self.extract_input_feature(selected_layer)
+            feature_geom: QgsGeometry = selected_feature.geometry()
+            geom: geometry.Polygon = wkt.loads(feature_geom.asWkt())
+            geom = orient(geom, -1)  # orient per QGS
+            bounds: tuple[float, float, float, float] = geom.bounds
+            width: float = int(abs(bounds[3] - bounds[1]))
+            height: float = int(abs(bounds[2] - bounds[0]))
+            base_raster: QgsRasterLayer = QgsRasterBlock(Qgis.Byte, width, height)
+            print(base_raster)
+            base_layer = QgsProject.instance().addMapLayer(base_raster, addToLegend=False)
+            # add raster layer group
+            layer_root = QgsProject.instance().layerTreeRoot()
+            layer_group = layer_root.addGroup("Simulation Outputs")
+            layer_group.addLayer(base_layer)
+        else:
+            QgsMessageLog.logMessage("no result")
--- futurb/futurb_dialog.py
+++ futurb/futurb_dialog.py
@@ -19,14 +19,35 @@
  ***************************************************************************/
 """
 
+import logging
 import os
 
 from qgis.PyQt import QtWidgets, uic
+from qgis.PyQt.QtWidgets import QDialogButtonBox, QLabel, QLineEdit, QListWidget, QPushButton, QSlider
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), "futurb_dialog_base.ui"))
+print(FORM_CLASS)
 
 
 class FuturbDialog(QtWidgets.QDialog, FORM_CLASS):
+
+    button_box: QDialogButtonBox
+    n_iterations: QLineEdit
+    isobenefit: QPushButton
+    classical: QPushButton
+    x_size: QLineEdit
+    y_size: QLineEdit
+    build_prob: QSlider
+    build_prob_val: QLabel
+    new_cent_p1: QLineEdit
+    new_cent_p2: QLineEdit
+    t_star: QLineEdit
+    random_seed: QLineEdit
+    layers_list: QListWidget
+
     def __init__(self, parent=None):
         """Constructor."""
         super(FuturbDialog, self).__init__(parent)
@@ -36,3 +57,14 @@ class FuturbDialog(QtWidgets.QDialog, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+        # prime
+        self.widget.addWidget
+        self.handle_build_prob()
+        # handle changes
+        self.build_prob.valueChanged.connect(self.handle_build_prob)
+
+    def handle_build_prob(self) -> None:
+        """ """
+        new_val: int = self.build_prob.value()
+        dec_val: float = new_val / 100
+        self.build_prob_val.setText(str(dec_val))
